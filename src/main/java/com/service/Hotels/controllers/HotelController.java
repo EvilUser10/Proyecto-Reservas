41acd43ef32b76bdc67e7766dd818c44999809e0
khalifa boulbayem <khalo_2@hotmail.com>
2024-03-12 16:04:37 +0000

    add CrossOrigin

--- src/main/java/com/service/Hotels/controllers/HotelController.java
+++ src/main/java/com/service/Hotels/controllers/HotelController.java
@@ -9,6 +9,7 @@ import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -26,6 +27,7 @@ import com.service.Hotels.exceptions.BadRequestException;
 
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/api/hotels")
 public class HotelController {
